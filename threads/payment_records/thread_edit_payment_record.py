f370d140cdddc42f0e8b0bf88755dd204b586488
vitorcarnieli <vitorcarnieli135@gmail.com>
2026-01-23 12:41:43 -0300

    refactor: turn thread that update status to update value too

--- threads/payment_records/thread_edit_payment_record.py
+++ threads/payment_records/thread_edit_payment_record.py
@@ -1,28 +1,22 @@
-from datetime import date
-
 from PySide6.QtCore import QObject, Signal, QThread
-from sqlalchemy import select, extract
 
 from dtos.message import Message
 from dtos.payment_record_dto import PaymentRecordDto
 from engine import Session
 from enums.message_type import MessageType
-from enums.payment_status import PaymentStatus
-from models import PaymentRecord
 from repositories.payment_record_repository import PaymentRecordRepository
-from repositories.student_repository import StudentRepository
 from services.payment_record_service import PaymentRecordService
 
 
-class ThreadEditPaymentRecordStatusSignals(QObject):
+class ThreadEditPaymentRecordSignals(QObject):
     signal_updated_record_dto = Signal(object)
     signal_message = Signal(Message)
 
 
-class ThreadEditPaymentRecordStatus(QThread):
+class ThreadEditPaymentRecord(QThread):
     def __init__(self, record_dto: PaymentRecordDto):
-        super(ThreadEditPaymentRecordStatus, self).__init__()
-        self.signals = ThreadEditPaymentRecordStatusSignals()
+        super(ThreadEditPaymentRecord, self).__init__()
+        self.signals = ThreadEditPaymentRecordSignals()
 
         self.session: Session = None
         self.record_dto = record_dto
@@ -34,8 +28,13 @@ class ThreadEditPaymentRecordStatus(QThread):
 
             self.payment_record_repository = PaymentRecordRepository(self.session)
             record_model = self.payment_record_repository.get_by_id(self.record_dto.id)
+
             record_model.payment_status = self.record_dto.payment_status
+            record_model.value = self.record_dto.value
+
+
             self.payment_record_repository.update(record_model)
+            self.session.commit()
 
             record_dto = PaymentRecordService.get_dto(record_model)
 
