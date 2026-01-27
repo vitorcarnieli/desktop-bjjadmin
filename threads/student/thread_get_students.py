e556c4c7b848dd3cc8fe7cb3b7038c558616bd1c
vitorcarnieli <vitorcarnieli135@gmail.com>
2026-01-27 15:46:18 -0300

    fix: bug when student does have payment records

--- threads/student/thread_get_students.py
+++ threads/student/thread_get_students.py
@@ -37,8 +37,11 @@ class ThreadGetStudents(QThread):
         self.lessons = self.lesson_repository.get_all()
         for dto in student_dtos:
             student = next((s for s in students if s.id == dto.id), None)
-            latest_record = self._most_recent_payment_record(student.payment_records)
-            dto.latest_payment_status = latest_record.payment_status
+            if not student.is_inactive:
+                latest_record = self._most_recent_payment_record(student.payment_records)
+                dto.latest_payment_status = latest_record.payment_status
+            else:
+                dto.latest_payment_status = None
             dto.frequency = self.get_frequency(dto)
 
 
