e12071ddb3d19178e88dc4cb364f663640751ec6
vitorcarnieli <vitorcarnieli135@gmail.com>
2026-02-03 14:12:21 -0300

    refactor: remove radio belts

--- views/student/add_student_view.py
+++ views/student/add_student_view.py
@@ -96,10 +96,6 @@ class StudentView(QDialog, Ui_AddStudentView):
         else:
             self.start_thread_add_student(student_dto)
 
-    def on_selection_change_radio_belts(self):
-        # TODO
-        return
-
     def on_click_label_perfil_photo(self):
         options = QFileDialog.Options()
         file, _ = QFileDialog.getOpenFileName(
